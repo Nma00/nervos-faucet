9999b878d5d70d0bd665b374b345b9a5e5ab860c
Nicolas Manrique <nicolas.manrique@cnaf.fr>
2022-04-19 13:29:43 +0200

    Update

--- scripts/deploy-script.js
+++ scripts/deploy-script.js
@@ -6,20 +6,16 @@
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  
+  const TokenA = await hre.ethers.getContractFactory("TokenA");
+  const tokenA = await TokenA.deploy();
+  await tokenA.deployed();
+  console.log("TokenA deployed to:", greeter.address);
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  const TokenB = await hre.ethers.getContractFactory("TokenB");
+  const tokenB = await TokenB.deploy();
+  await tokenB.deployed();
+  console.log("TokenB deployed to:", greeter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- src/FaucetApp.js
+++ src/FaucetApp.js
@@ -1,8 +1,23 @@
 import './FaucetApp.css';
 
+function getTokens() {
+  console.log('Get tokens !!!!', document.getElementById('godwoken-address').value);
+}
+
 function FaucetApp() {
   return (
-      <h1>Nervos Swap Faucet</h1>
+    <div className="py-3">
+      <h1 className="mb-4">Nervos Swap Faucet</h1>
+      <p className="lead mb-4">Get test tokens for <a className="text-decoration-none" href="http://nervoswap.freeddns.org:8080">Nervos Swap</a>.</p>
+      <div className="mb-3">
+        <label htmlFor="godwoken-address" className="form-label">Your Godwoken Testnet address</label>
+        <input type="text" className="form-control" id="godwoken-address" placeholder="" />
+      </div>
+      <div className="d-grid mb-4">
+          <button type="button" className="btn btn-primary" onClick={getTokens}>Get 10000 TOKENA and 10000 TOKENB</button>
+      </div>
+      <button type="button" className="btn btn-link btn-sm">Add TOKENA to Metamask</button><button type="button" className="btn btn-link btn-sm">Add TOKENB to Metamask</button>      
+    </div>
   );
 }
 
